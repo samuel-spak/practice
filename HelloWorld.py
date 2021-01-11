85a5cc83e14ffab3feba37160066b079d9e418e4
samuel-spak <77252931+samuel-spak@users.noreply.github.com>
2021-01-10 21:24:10 -0500

    Book been made

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,12 @@
 print("hello world")
 
 print("goodbye world")
+
+class Book():
+    def __init__(self, pages, author):
+        self.pages = pages
+        self.author = author
+
+harryPotter = Book(400, "J.K. Rowling")
+
+print(harryPotter.pages)
